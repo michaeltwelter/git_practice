61fdc2b73159e2744602b53049aa979b4bf078ee
michaeltwelter <73920131+michaeltwelter@users.noreply.github.com>
2022-03-19 18:06:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.metrics import accuracy_score
